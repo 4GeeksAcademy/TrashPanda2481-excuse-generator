6987753511dff23cc5ce75eb2180fa402ec06638
TrashPanda2481 <154713431+TrashPanda2481@users.noreply.github.com>
2025-07-20 21:32:34 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+function generateExcuse(){
+  var who = ['The dog ','My grandma ','The mailman ','My bird '];
+  var action = ['ate ','peed on ','crushed ','broke '];
+  var what = ['my homework ','my phone ','the car '];
+  var when = ['before class','while I was sleeping','while I was exercising','during my lunch','while I was praying'];
+
+  var whoPart =who[Math.floor(Math.random() * who.length)];
+  var actionPart = action[Math.floor(Math.random() * action.length)];
+  var whatPart =what[Math.floor(Math.random() * what.length)];
+  var whenPart =when[Math.floor(Math.random() * when.length)];
+  return whoPart+''+actionPart+''+whatPart+''+whenPart;
+}
+function showExcuse() {
+  var excuseElement = document.getElementById('excuse');
+  excuseElement.innerHTML=generateExcuse();
+}
+document.addEventListener('DOMContentLoaded', function () {
+  showExcuse(); // initial excuse
+
+  var button = document.getElementById('generate-btn');
+  button.addEventListener('click', showExcuse);
+});
   console.log("Hello Rigo from the console!");
-};
+
